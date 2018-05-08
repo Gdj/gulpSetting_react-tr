2b34c76e9b54ca382bc890e8696f5b40773f9f5a
Gdj <turfrain@yahoo.co.kr>
2018-05-08 18:14:40 +0900

    first commit

---
+++ root/dev_inc/js/jsx/hello.jsx
@@ -0,0 +1,6 @@
+const element = <h1> Hello, react.jsx? </h1>;
+ 
+ReactDOM.render(
+  element, 
+  document.getElementById('react')
+);
---
+++ root/inc/js/react/hello.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var element = React.createElement(
+  'h1',
+  null,
+  ' Hello, react.jsx? '
+);
+
+ReactDOM.render(element, document.getElementById('react'));
